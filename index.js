a6aaae0ae9cb16ba438509d3a2a752e692f002ad
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-24 13:07:28 -0500

    commit tree e80c0553ac6e4f0f5976054291c9432bb174d357

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+function destructivelyAppendCat(ralph) {
+    return cats.push("Ralph");
+};
+
+function destructivelyPrependCat(bob) {
+    return cats.unshift("Bob");
+};
+
+function destructivelyRemoveLastCat(garfield) {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat(milo) {
+    return cats.shift();
+};
+
+function appendCat(broom) {
+    return [...cats, "Broom"];
+};
+
+function prependCat(arnold) {
+    return ["Arnold", ...cats];
+};
+
+function removeLastCat(garfield) {
+    return cats.slice(0,2);
+};
+
+function removeFirstCat(milo) {
+    return cats.slice(1);
+};
